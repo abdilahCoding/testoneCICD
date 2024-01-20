e969d32f6ab8d5fb921661974b13ca5dbe0c63ba
abdilah Elaidi <abdilahelaidii@gmail.com>
2024-01-20 18:26:12 +0100

    users

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = 5000;
 app.get('/', (req, res) => {
   res.send('heloo world')
 })
+app.get('/user', (req, res) => {
+  res.send('heloo users')
+})
 
 app.listen(PORT, () => {
   console.log("Server running");
